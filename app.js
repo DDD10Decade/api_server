a5cca857fec4a8b267ecf1766b223cb2152f65c0
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-08-30 22:04:37 +0800

    初始

---
+++ app.js
@@ -0,0 +1,11 @@
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+
+// write your code here...
+
+// 调用 app.listen 方法，指定端口号并启动web服务器
+app.listen(3007, function () {
+    console.log('api server running at http://127.0.0.1:3007')
+})
