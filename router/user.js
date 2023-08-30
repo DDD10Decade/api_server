72ec90351e1a62ccf7e4bd293ac1d46365d6a9f9
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-08-30 22:38:23 +0800

    mysql 模块

--- router/user.js
+++ router/user.js
@@ -15,6 +15,10 @@ const router = express.Router()
 // // 将路由对象共享出去
 // module.exports = router
 
+/**
+ * 目的：为了保证 路由模块 的纯粹性，所有的 路由处理函数 ，必须抽离到对应的 路由处理函数
+模块 中
+ */
 // 导入用户路由处理函数模块
 const userHandler = require('../router_handler/user')
 
